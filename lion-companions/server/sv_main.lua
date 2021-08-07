b8f4ccc00d2f5167b0ab0f1973c9cb59b2636571
Lionh34rt <76415140+Lionh34rt@users.noreply.github.com>
2021-08-07 10:51:44 +0200

    Update sv_main.lua

--- lion-companions/server/sv_main.lua
+++ lion-companions/server/sv_main.lua
@@ -4,44 +4,8 @@ TriggerEvent('QBCore:GetObject', function(obj)
     QBCore = obj 
 end)
 
-QBCore.Functions.CreateUseableItem("pet_shepherd", function(source, item)
+QBCore.Functions.CreateUseableItem("doggo", function(source, item)
 	local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-  
-	TriggerClientEvent("lion-companions:client:ToggleCompanion", src, "a_c_shepherd")
-end)
-
-QBCore.Functions.CreateUseableItem("pet_cat", function(source, item)
-	local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-  
-	TriggerClientEvent("lion-companions:client:ToggleCompanion", src, "a_c_cat_01")
-end)
-
-QBCore.Functions.CreateUseableItem("pet_pug", function(source, item)
-	local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-  
-	TriggerClientEvent("lion-companions:client:ToggleCompanion", src, "a_c_pug")
-end)
-
-QBCore.Functions.CreateUseableItem("pet_retriever", function(source, item)
-	local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-  
-	TriggerClientEvent("lion-companions:client:ToggleCompanion", src, "a_c_retriever")
-end)
-
-QBCore.Functions.CreateUseableItem("pet_rat", function(source, item)
-	local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-  
-	TriggerClientEvent("lion-companions:client:ToggleCompanion", src, "a_c_rat")
-end)
-
-QBCore.Functions.CreateUseableItem("pet_mtlion", function(source, item)
-	local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-  
-	TriggerClientEvent("lion-companions:client:ToggleCompanion", src, "a_c_mtlion")
+    	local Player = QBCore.Functions.GetPlayer(src)
+	TriggerClientEvent("lion-companions:client:ToggleCompanion", src)
 end)
